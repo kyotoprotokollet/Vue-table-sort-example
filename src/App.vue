ae0377bd4a160a0badce8a9d5606a409319fd342
Johan Dahl <johdah@bredband2.se>
2019-04-12 15:25:18 +0200

    Reformated some data. Changed some styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
       <div class="tickets">
-            <Ticket v-for="ticket in tickets" v-bind:key="ticket.id" :ticket="ticket"/>
+            <Ticket v-for="ticket in tickets" :key="ticket.id" :ticket="ticket"/>
       </div>
   </div>
 </template>
@@ -11,24 +11,24 @@ import axios from 'axios'; // We use Axios to fetch our data
 import Ticket from './components/Ticket.vue';
 
 export default {
-  name: 'app',
-  data() {
-    return {
-        loading: true,
-        tickets: [],
-        customers: [],
-        pagination: {
-            totalPages: 1,
-            itemsPerPage: 25,
-            currentPage: 1,
-        },
-        errors: []
-    };
-  },
-  components: {
-    Ticket
-  },
-  mounted() {
+    name: 'app',
+    data() {
+        return {
+            loading: true,
+            tickets: [],
+            customers: [],
+            pagination: {
+                totalPages: 1,
+                itemsPerPage: 25,
+                currentPage: 1,
+            },
+            errors: []
+        }
+    },
+    components: {
+        Ticket
+    },
+    mounted() {
         // Use Axios to make an ajax request to our local data.
         axios.get('./data/data.json')
         .then(response => {
@@ -39,29 +39,30 @@ export default {
             // Fill our errors array with any errors
             this.errors.push(e)
         })
-  }
+    }
 }
 </script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Lato:300,400,700,900');
+@import "./src/assets/scss/_globals.scss";
 
 body {
-    background: rgb(224, 236, 235);
+    background-color: $color-egg;
 }
 
 .tickets {
-    padding: 30px;
-    max-width: 1200px;
+    padding: 2.5vw;
+    max-width: 1500px;
     margin: 0 auto;
+    font-size: 15px;
 }
 
 #app {
   font-family: 'Lato', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $color-text;
   margin-top: 60px;
 }
 </style>
--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -1,33 +1,167 @@
 <template>
   <div class="ticket">
-    <div class="ticket-section ticket-id">{{ ticket.id }}</div>
-    <div class="ticket-section ticket-status">{{ ticket.status }}</div>
-    <div class="ticket-section ticket-requestedAmount">{{ ticket.requested_amount }}</div>
-    <div class="ticket-section ticket-grantedAmount">{{ ticket.granted_amount }}</div>
-    <div class="ticket-section ticket-startDate">{{ ticket.start_date }}</div>
-    <div class="ticket-section ticket-lastActionDate">{{ ticket.last_action_date }}</div>
-    <div class="ticket-section ticket-customer">{{ ticket.customer.name }}</div>
-    <div class="ticket-section ticket-button">...</div>
+    <div class="ticket-section ticket-section--id" data-label="Ärendenummer">
+        <!-- I imagine this link leading to another view with details about this ticket -->
+        # <a href="#">{{ ticket.id }}</a>
+    </div>
+    <div class="ticket-section" data-label="Kund">
+        {{ ticket.customer.name }}
+    </div>
+    <div class="ticket-section align-right" data-label="Ansökt belopp">
+        {{ formatNumber(ticket.requested_amount) }}
+    </div>
+    <div class="ticket-section align-right" data-label="Beviljat belopp">
+        {{ formatNumber(ticket.granted_amount) }}
+    </div>
+    <div class="ticket-section align-right" data-label="Påbörjat">
+        <span class="ticket-date">
+            {{ formatDate(ticket.created_at) }}
+        </span>
+    </div>
+    <div class="ticket-section align-right" data-label="Senaste handling">
+        <span class="ticket-date">
+            {{ formatDate(ticket.updated_at) }}
+        </span>
+    </div>
+    <div class="ticket-section align-center" data-label="Status">
+        <span class="badge" :class="ticket.status">
+            {{ ticket.status }}
+        </span>
+    </div>
+    <div class="ticket-section">
+        <button class="button--edit">Redigera</button>
+    </div>
   </div>
 </template>
 
 <script>
+// Use the moment library to format the time
+import moment from 'moment';
+
 export default {
     name: 'Ticket',
-    props: ['ticket']
+    props: {
+        ticket: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            // Spread props to data
+            ...this.ticket
+        }
+    },
+    methods: {
+        // Format the date
+        formatDate(date) {
+            return moment(date).format('YYYY-MM-DD')
+        },
+        // Format the number to a more human readable version, omit decimals
+        formatNumber(number) {
+            return number.toLocaleString('sv-SE', { 
+                style: 'currency', 
+                currency: 'SEK' ,
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+            })
+        }
+    }
   }
 </script>
 
+
+
 <style lang="scss" scoped>
+@import "./src/assets/scss/_globals.scss";
+
 .ticket {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
     background-color: white;
-    padding:  20px 10px;
-    font-size: 15px;
+    padding: 20px;
+    
+    @include respond-above("medium") {
+        display: grid;
+        grid-template-columns: 2fr repeat(6, 3fr) max-content;
+    }
 
     & + .ticket {
         margin-top: 10px;
     }
 }
+
+.ticket-section {
+    padding: 7px;
+    border-bottom: 1px solid #ebebeb;
+    font-size: 80%;
+    display: flex;
+    font-variant: common-ligatures tabular-nums;
+
+    &[data-label] {
+        &:before {
+            content: attr(data-label);
+            width: 50%;
+        }
+    }
+
+    @include respond-above("medium") {
+        display: block;
+        padding: 0 10px;
+        border-bottom: none;
+        position: relative;
+        font-size: 100%;
+        align-self: center;
+
+        &:after {
+            content: '';
+            display: block;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            width: 2px;
+            background-color: #ebebeb;
+        }
+
+        &[data-label] {
+            &:before {
+                content: '';
+            }
+        }
+
+        &.align-right {
+            text-align: right;
+        }
+
+        &.align-center {
+            text-align: center;
+        }
+    }
+}
+
+.ticket-section--id {
+    @include respond-above("medium") {
+        font-size: 90%;
+    }
+}
+
+.badge {
+    padding: 7px 10px;
+    background-color: rgb(49, 49, 49);
+    color: white;
+    border-radius: 10px;
+
+    &.active {
+        background-color: #2e85cc;
+    }
+
+    &.completed {
+        background-color: #2ecc71;
+    }
+}
+
+.button--edit {
+    padding: 5px;
+}
+
+
 </style>
